4e460438e9fd80a2607b6bf0c655b04351151c98
Conor Giffen-McCloskey <conorgiffenmccloskey@gmail.com>
2024-09-13 21:20:32 +0100

    update and refactor existing domain and controller tests within the project context (additional tests to be added)

--- project/src/test/java/example/project/api/ProjectControllerTests.java
+++ project/src/test/java/example/project/api/ProjectControllerTests.java
@@ -31,9 +31,9 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @AutoConfigureMockMvc
 //Restaurant api requires a valid identity for any request to work
 public class ProjectControllerTests {
-    private String API_BASE_URL = "http://localhost:8901/restaurants/";
+    private String API_BASE_URL = "http://localhost:8901/project/";
     private String MOCK_ADMIN_TOKEN = "Admin";
-    private String VALID_RESTAURANT_ID = "1234";
+    private String VALID_PROJECT_ID = "1234";
 
     private MockMvc mockMvc;
     private ProjectQueryHandler projectQueryHandler;
@@ -43,7 +43,7 @@ public class ProjectControllerTests {
     private Project createValidRestaurantWithTasks(){
         List<BaseTask> menuItems = new ArrayList<>();
         //Need to use the factory method (not constructor)
-        Project project =  Project.projectOf(VALID_RESTAURANT_ID,"The Balti");
+        Project project =  Project.projectOf(VALID_PROJECT_ID,"project name");
         project.addTask(createValidMenuItem());
         return project;
     }
@@ -68,23 +68,23 @@ public class ProjectControllerTests {
     }
 
     @Test
-    @DisplayName("Pass a valid restaurant id to view a particular restaurant's details, display the response in JSON")
+    @DisplayName("Pass a valid project id to view a particular project's details, display the response in JSON")
     void test01() throws Exception {
         //Return type from RestaurantQueryHandler for getRestaurant is GetRestaurantResponse
         GetProjectResponse restaurantResponse = new GetProjectResponse();
-        restaurantResponse.setProjectId(VALID_RESTAURANT_ID);
-        restaurantResponse.setName("Restaurant");
+        restaurantResponse.setProjectId(VALID_PROJECT_ID);
+        restaurantResponse.setName("project");
         //mock behaviour
         when(identityService.isAdmin(MOCK_ADMIN_TOKEN)).thenReturn(true);
-        when(projectQueryHandler.getProject(VALID_RESTAURANT_ID)).thenReturn(Optional.of(restaurantResponse));
+        when(projectQueryHandler.getProject(VALID_PROJECT_ID)).thenReturn(Optional.of(restaurantResponse));
 
         //Check the format in generated_requests to ensure the json path keys are correct
-        mockMvc.perform(get(API_BASE_URL.concat(VALID_RESTAURANT_ID))
+        mockMvc.perform(get(API_BASE_URL.concat(VALID_PROJECT_ID))
                         .header("Authorization", MOCK_ADMIN_TOKEN))
                 .andDo(print())
                 .andExpect(status().isOk())
                 .andExpect(content().contentType(MediaType.APPLICATION_JSON))
-                .andExpect(jsonPath("restaurantId").value(restaurantResponse.getProjectId()))
+                .andExpect(jsonPath("projectId").value(restaurantResponse.getProjectId()))
                 .andExpect(jsonPath("name", equalTo(restaurantResponse.getName()))
                 );
     }
@@ -109,9 +109,9 @@ public class ProjectControllerTests {
                 .andExpect(jsonPath("$[0].id").value(restaurant.getId()))
                 .andExpect(jsonPath("$[0].name", equalTo(restaurant.getName())))
                 //expected 1L was 1 (so need to cast from long to int
-                .andExpect(jsonPath("$[0].menuItems[0].id", equalTo((int) restaurant.getTasks().get(0).getId())))
-                .andExpect(jsonPath("$[0].menuItems[0].name", equalTo(restaurant.getTasks().get(0).getName())))
-                .andExpect(jsonPath("$[0].menuItems[0].price", equalTo(restaurant.getTasks().get(0).getHours()))
+                .andExpect(jsonPath("$[0].tasks[0].id", equalTo((int) restaurant.getTasks().get(0).getId())))
+                .andExpect(jsonPath("$[0].tasks[0].name", equalTo(restaurant.getTasks().get(0).getName())))
+                .andExpect(jsonPath("$[0].tasks[0].hours", equalTo(restaurant.getTasks().get(0).getHours()))
                 );
     }
 
@@ -120,28 +120,30 @@ public class ProjectControllerTests {
     void test03() throws Exception {
         //Return type from RestaurantQueryHandler for getRestaurantMenu is GetRestaurantMenuResponse
         GetProjectTaskResponse restaurantMenuResponse = new GetProjectTaskResponse();
-        restaurantMenuResponse.setProjectId(VALID_RESTAURANT_ID);
-        restaurantMenuResponse.setName("Restaurant");
+        restaurantMenuResponse.setProjectId(VALID_PROJECT_ID);
+        restaurantMenuResponse.setName("project");
         restaurantMenuResponse.setTasks(List.of(createValidMenuItem()));
         //mock behaviour
         when(identityService.isAdmin(MOCK_ADMIN_TOKEN)).thenReturn(true);
-        when(projectQueryHandler.getProjectTasks(VALID_RESTAURANT_ID)).thenReturn(Optional.of(restaurantMenuResponse));
+        when(projectQueryHandler.getProjectTasks(VALID_PROJECT_ID)).thenReturn(Optional.of(restaurantMenuResponse));
 
         //Check the format in generated_requests to ensure the json path keys are correct
-        mockMvc.perform(get(API_BASE_URL.concat("menuItems/" + VALID_RESTAURANT_ID))
+        mockMvc.perform(get(API_BASE_URL.concat("task/" + VALID_PROJECT_ID))
                         .header("Authorization", MOCK_ADMIN_TOKEN))
                 .andDo(print())
                 .andExpect(status().isOk())
                 .andExpect(content().contentType(MediaType.APPLICATION_JSON))
-                .andExpect(jsonPath("restaurantId").value(restaurantMenuResponse.getProjectId()))
+                .andExpect(jsonPath("projectId").value(restaurantMenuResponse.getProjectId()))
                 .andExpect(jsonPath("name", equalTo(restaurantMenuResponse.getName())))
                 //expected 1L was 1 (so need to cast from long to int
-                .andExpect(jsonPath("menuItems[0].id", equalTo((int) restaurantMenuResponse.getTasks().get(0).getId())))
-                .andExpect(jsonPath("menuItems[0].name", equalTo(restaurantMenuResponse.getTasks().get(0).getName())))
-                .andExpect(jsonPath("menuItems[0].price", equalTo(restaurantMenuResponse.getTasks().get(0).getHours()))
+                .andExpect(jsonPath("tasks[0].id", equalTo((int) restaurantMenuResponse.getTasks().get(0).getId())))
+                .andExpect(jsonPath("tasks[0].name", equalTo(restaurantMenuResponse.getTasks().get(0).getName())))
+                .andExpect(jsonPath("tasks[0].hours", equalTo(restaurantMenuResponse.getTasks().get(0).getHours()))
                 );
     }
 
     //to be added - post request to add a restaurant (with command details)
     //invalid requests to each end point
+    // TODO - Add tests for the post request to add a project and tasks
+    // TODO - Add tests for invalid scenarios of each endpoint
 }
