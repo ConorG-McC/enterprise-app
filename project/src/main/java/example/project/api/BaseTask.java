3fcb281da45597c0dc78cdad33eda4aee59797cb
Conor Giffen-McCloskey <conorgiffenmccloskey@gmail.com>
2024-09-14 02:54:27 +0100

    Add publish event functionality for project context when new project is created.

--- project/src/main/java/example/project/api/BaseTask.java
+++ project/src/main/java/example/project/api/BaseTask.java
@@ -1,8 +1,10 @@
 package example.project.api;
 
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
 import example.common.domain.Hours;
 
 //Used in AddNewMenuCommand to avoid coupling of domain MenuItem to api
+
 public interface BaseTask {
      long id();
 
