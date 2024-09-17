ed745f1615b9111c3f6ff0478547760abac5ae4b
Conor Giffen-McCloskey <conorgiffenmccloskey@gmail.com>
2024-09-18 00:04:42 +0100

    Add test TODO's and placeholders across modules

---
+++ assignment/src/test/java/example/assignment/AssignmentTestSuite.java
@@ -0,0 +1,25 @@
+package example.assignment;
+
+import example.assignment.api.AssignmentControllerTests;
+import example.assignment.api.ProjectControllerTest;
+import example.assignment.domain.ProjectTests;
+import example.assignment.domain.TaskAssignmentLineItemTests;
+import example.assignment.domain.TaskAssignmentTests;
+import example.assignment.domain.TaskTests;
+import org.junit.platform.suite.api.SelectClasses;
+import org.junit.platform.suite.api.Suite;
+
+@Suite
+@SelectClasses(
+        {
+                ProjectTests.class,
+                TaskTests.class,
+                TaskAssignmentTests.class,
+                TaskAssignmentLineItemTests.class,
+                ProjectControllerTest.class,
+                AssignmentControllerTests.class
+
+        }
+        )
+public class AssignmentTestSuite {
+}
